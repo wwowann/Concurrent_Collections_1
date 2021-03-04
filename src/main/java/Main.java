62f4b60314e2a36c276158773f14ef717324156c
wwowann <ivasin75@mail.ru>
2021-03-04 13:42:26 +0300

    add all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,24 @@
 import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        final int quentityBell = 10;
-        Queue<String> phoneNumers = new ConcurrentLinkedQueue<>();
+        final int quentityBell = 20;
+        final int queueSize = 10;
+        BlockingQueue<String> phoneNumers = new ArrayBlockingQueue<>(queueSize);
         Atc atc = new Atc();
         Thread thread = new Thread(() -> {
             for (int i = 0; i < quentityBell; i++) {
-                phoneNumers.add(atc.numberPhone());
+                try {
+                    phoneNumers.put(atc.numberPhone());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
@@ -28,21 +36,28 @@ public class Main {
         thread.join();
     }
 
-    public static void metod(Queue<String> phoneNumers, Thread thread) {
+    public static void metod(BlockingQueue<String> phoneNumers, Thread thread) {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        while (thread.isAlive() || phoneNumers.size()>0){
-            System.out.println("Оператор " + Thread.currentThread().getName() +
-                    " обрабатывает звонок по номеру " + phoneNumers.poll());
-            Random random = new Random();
-            try {
-                Thread.sleep(random.nextInt(5) * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            while (thread.isAlive() || !phoneNumers.isEmpty()) {
+
+                System.out.println("Оператор " + Thread.currentThread().getName() +
+                        " обрабатывает звонок по номеру " + phoneNumers.poll());
+                Random random = new Random();
+                try {
+                    Thread.sleep(random.nextInt(5) * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        } finally {
+            lock.unlock();
         }
         System.out.println(Thread.currentThread().getName() + " Звонков в очереди нет");
     }
